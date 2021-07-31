e0b14261836a624a06572fe37ce3337dcd3510ff
avrpic8 <avrpic8@gmail.com>
2021-08-01 00:33:29 +0430

    Autoload.php fixed in ubuntu os

--- system/bootstrap/Autoload.php
+++ system/bootstrap/Autoload.php
@@ -4,25 +4,17 @@ namespace System\Bootstrap;
 
 class Autoload{
 
-    private function updateLast(&$array, $value){
-        array_pop($array);
-        array_push($array, $value);
-    }
-
     public function autoloader(){
 
         spl_autoload_register(function($className){
+
             $className = strtolower(str_replace("\\" , DIRECTORY_SEPARATOR, $className));
             $className = preg_split("#/#", $className);
-
             $last = array_pop($className);
             array_push($className, ucfirst($last));
-            //var_dump($className);
-            //var_dump($className);
+
             $className = implode("/", $className);
             include_once $_SERVER['DOCUMENT_ROOT'] . '/tinyMVC/' . $className . '.php';
         });
     }
-
-
 }
