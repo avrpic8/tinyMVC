5de2e7c5c8664c5be61037aedc6a3505b24099ca
saeed <avrpic8@gmail.com>
2021-07-31 18:51:28 +0430

    fixing Autoload.php class part2

--- system/bootstrap/Autoload.php
+++ system/bootstrap/Autoload.php
@@ -4,15 +4,25 @@ namespace System\Bootstrap;
 
 class Autoload{
 
+    private function updateLast(&$array, $value){
+        array_pop($array);
+        array_push($array, $value);
+    }
+
     public function autoloader(){
 
         spl_autoload_register(function($className){
             $className = strtolower(str_replace("\\" , DIRECTORY_SEPARATOR, $className));
-            //$className = preg_split("#/#", $className);
+            $className = preg_split("#/#", $className);
 
+            $last = array_pop($className);
+            array_push($className, ucfirst($last));
             //var_dump($className);
-            //var_dump(implode("/", $className));
+            //var_dump($className);
+            $className = implode("/", $className);
             include_once $_SERVER['DOCUMENT_ROOT'] . '/tinyMVC/' . $className . '.php';
         });
     }
+
+
 }
