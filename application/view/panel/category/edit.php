086b705c633324d2f77cc355b016ab3b74e65b3f
saeed <avrpic8@gmail.com>
2021-08-01 18:02:35 +0430

    category dynamic

--- application/view/panel/category/edit.php
+++ application/view/panel/category/edit.php
@@ -1,13 +1,16 @@
 <?php $this->include("panel.layouts.header");?>
 
-                <form>
+                <form accept-charset="<?php $this->url('category/update/' . $category['id']); ?>" method="post">
                     <section class="form-group">
                         <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" placeholder="name ...">
+                        <input value="<?php echo $category['name']; ?>" type="text" class="form-control" id="name"
+                               name="name"
+                               placeholder="name ...">
                     </section>
                     <section class="form-group">
                         <label for="description">Description</label>
-                        <input type="text" class="form-control" id="description" placeholder="description ...">
+                        <input type="text" class="form-control" id="description" name="description"
+                               placeholder="description ..." value="<?php echo $category['description']; ?>">
                     </section>
                     <button type="submit" class="btn btn-primary">Update</button>
                 </form>
